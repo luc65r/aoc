62624e97efcc1d668ad20ab435b415535ca348cd
Lucas Ransan <lucas@ransan.tk>
2020-12-07 12:10:50 +0100

    update flake.nix

--- flake.nix
+++ flake.nix
@@ -9,10 +9,21 @@
     }; stdenv.mkDerivation {
       name = "aoc";
       src = self;
-      buildPhase = "";
-      installPhase = "";
 
-      buildInputs = [
+      dontConfigure = true;
+      dontInstall = true;
+      doCheck = true;
+
+      buildPhase = ''
+        rm ./2020/01/report.nix # fails to patch shebang
+        patchShebangs .
+      '';
+
+      checkPhase = ''
+        ./test.sh
+      '';
+
+      nativeBuildInputs = [
         gcc
         ghc
         zig
