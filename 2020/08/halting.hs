5d50754f44dadcdc2ddd2efcca8e158d0cf3fd2a
Lucas Ransan <lucas@ransan.tk>
2020-12-09 20:59:32 +0100

    use IntMap instead op Array
    slightly fasted on modifications

--- 2020/08/halting.hs
+++ 2020/08/halting.hs
@@ -4,6 +4,7 @@
 import Text.Read hiding (step, get)
 import Data.Array
 import Data.Monoid
+import qualified Data.IntMap as M
 import Control.Monad.RWS
 import Control.Arrow
 
@@ -33,7 +34,7 @@ data Status = Running | Loop | Terminated
 
 main = do insts <- (map read . lines) <$> getContents
           let len = length insts - 1
-          putStrLn . show . getSum . snd . execRWS run (listArray (0, len) insts) $ (0, listArray (0, len) $ repeat False)
+          putStrLn . show . getSum . snd . execRWS run (listArray (0, len) insts) $ (0, M.fromDistinctAscList . zip [0..len] . repeat $ False)
 
 
 exec :: Instruction -> RWS a (Sum Int) (Int, b) ()
@@ -41,17 +42,17 @@ exec (Acc n) = tell (Sum n) >> modify (first (+ 1))
 exec (Nop _) = modify $ first (+ 1)
 exec (Jmp n) = modify $ first (+ n)
 
-step :: RWS (Array Int Instruction) (Sum Int) (Int, Array Int Bool) Status
+step :: RWS (Array Int Instruction) (Sum Int) (Int, M.IntMap Bool) Status
 step = do
     (p, e) <- get
     insts <- ask
     if | p > (snd . bounds $ insts) -> return Terminated
-       | e ! p -> return Loop
+       | e M.! p -> return Loop
        | otherwise -> do
-           put (p, e // [(p, True)])
+           put (p, M.adjust (const True) p e)
            exec $ insts ! p
            return Running
 
-run :: RWS (Array Int Instruction) (Sum Int) (Int, Array Int Bool) Status
+run :: RWS (Array Int Instruction) (Sum Int) (Int, M.IntMap Bool) Status
 run = step >>= \case Running -> run
                      other -> return other
