87fa8fdae849376cebe57ecb3447c39431ed5ba9
Lucas Ransan <lucas@ransan.tk>
2020-12-09 16:29:12 +0100

    cleaner state

--- 2020/09/encoding.hs
+++ 2020/09/encoding.hs
@@ -1,9 +1,11 @@
 import Data.Monoid
+import Data.List
 import Control.Monad.State
 
 main = do input <- (map read . lines) <$> getContents
           let ns = firstNonSum $ input
-              l = evalState (findEq ns input) (0, (0, 0), False)
+              (a, n) = evalState (findEq ns input) []
+              l = take n . drop a $ input
           putStrLn . show $ ns
           putStrLn . show $ maximum l + minimum l
 
@@ -13,19 +15,14 @@ firstNonSum xs
     | otherwise = x
     where x = xs !! 25
 
-findEq :: Int -> [Int] -> State (Int, (Int, Int), Bool) [Int]
-findEq n xs = do
-    (s, (a, b), l) <- get
-    case compare s n of
-      EQ -> return . take (b - a) . drop a $ xs
-      LT -> do
-          put (s + xs !! b, (a, b + 1), False)
-          findEq n xs
-      GT -> do
-          if l
-             then put (s - xs !! (b - 1), (a, b - 1), True)
-             else put (s - xs !! a, (a + 1, b), True)
-          findEq n xs
+findEq :: Int -> [Int] -> State [Maybe Int] (Int, Int)
+findEq n (x:xs) = do
+    modify $ map (mfilter (<= n) . fmap (+ x)) . (return 0 :)
+    m <- get
+    case findIndex (any (== n)) m of
+        Just i -> let a = length m - i - 1
+                   in return (a, i)
+        Nothing -> findEq n xs
 
 isSum :: Int -> [Int] -> Bool
 isSum n = any ((== n) . uncurry (+)) . pairs
