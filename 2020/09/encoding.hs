6bbe8ac29bbd1910402abe62d4cec82cb57e3ef2
Lucas Ransan <lucas@ransan.tk>
2020-12-09 09:04:23 +0100

    ninth day

---
+++ 2020/09/encoding.hs
@@ -0,0 +1,22 @@
+import Data.List
+
+main = do input <- (map read . lines) <$> getContents
+          let ns = firstNonSum $ input
+          putStrLn . show $ ns
+          putStrLn . show . findC ns $ input
+
+firstNonSum :: [Int] -> Int
+firstNonSum xs
+    | isSum x (take 25 xs) = firstNonSum $ tail xs
+    | otherwise = x
+    where x = xs !! 25
+
+isSum :: Int -> [Int] -> Bool
+isSum n = any ((== n) . sum) . filter ((== 2) . length) . subsequences
+
+findC :: Int -> [Int] -> Int
+findC n xs
+    | last s == n = minimum range + maximum range
+    | otherwise = findC n (tail xs)
+    where s = takeWhile (<= n) . scanl (+) 0 $ xs
+          range = take (length s - 1) xs
